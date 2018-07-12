5cf3797a86d95f3cb8180a63f7acd746bbedb35e
schnell7 <schnell7@students.uni-marburg.de>
2018-07-12 18:21:47 +0200

    first

---
+++ sauber/master.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import servo_lidar_test2 as sl
+from lidar_lite import Lidar_Lite
+
+import script_temp as st
+
+import time
+import datetime
+
+
+
+
+# setup
+#ich bin mir unsicher ob beim importieren die ganzen Variablen mitimportiert werden oder nicht
+lidar = Lidar_Lite()
+connected = lidar.connect(1)
+if connected < -1:
+  print( "Not Connected")
+
+
+pin_temp = 4
+#18 , 23 oder 24
+servoPIN1 = 18
+servoPIN2 = 23
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pin_temp, GPIO.IN)
+
+
+GPIO.setup(servoPIN1, GPIO.OUT)
+GPIO.setup(servoPIN2, GPIO.OUT)
+
+
+p1 = GPIO.PWM(servoPIN1, 50) # GPIO  als PWM mit 50Hz
+p2 = GPIO.PWM(servoPIN2, 50) # GPIO  als PWM mit 50Hz
+
+p1.start(2.5) # Initialisierung
+p2.start(2.5) # Initialisierung
+
+lid_data = []
+temp_data = []
+
+while True:
+    #vielleicht auch mehr Zyklen
+    for i in range(10):
+        lid_data.append(sl.move())
+        temp_data.append( [datetime.datetime.now(), st.temp_messen()] )
+    save_lid = open("PFAD/ZUR/DATEI/lidar_data.txt", "a")
+    save_lid.write(lid_data)
+    save_lid.close()
+    save_temp = open("PFAD/ZUR/DATEI/temp_data.txt", "a")
+    save_temp.write(temp_data)
+    save_temp.close()
---
+++ sauber/readme.txt
@@ -0,0 +1,25 @@
+idee:
+
+dateien dieses ordners zusammenlassen
+
+
+nur die datei master.py wird ausgeführt, am besten aus dem autostart. dazu
+
+    /etc/rc.local   öffnen
+vor der zeile "exit 0"
+    sudo python PFAD/ZUR/DATEI/master.py 
+hinzufügen
+
+
+die camera funktioniert vermittels cronjob, falls noch nicht eingerichtet 
+https://raspberry.tips/raspberrypi-einsteiger/cronjob-auf-dem-raspberry-pi-einrichten
+
+
+[12.07.18 18:17]
+todo:
+
+verkabelung checken, und im script anpassen
+
+sicherlich muss noch ein bisschen angepasst werden, ist nicht auf dem raspi getestet worden 
+
+methode finden um funktionen gleichzeitig ausführen zu lassen, ggf auf den master.py ansatz verzichten und im autostart die temp und lidar funktionen einzeln starten 
